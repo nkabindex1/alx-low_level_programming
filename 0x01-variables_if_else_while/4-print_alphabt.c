cf5235a1fa11c3a06300605fc08cb74a7c649c55
Xolani <nkabindex1@gmail.com>
2023-06-18 13:34:31 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - point of entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int length = sizeof(alphabets) / sizeof(alphabets[0]);
+	int i;
+
+	for (i = 0; i < length - 1; i++)
+	{
+		char lowerCh = tolower(alphabets[i]);
+		if(lowerCh != 'q' || lowerCh != 'e')
+			putchar(lowerCh);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
