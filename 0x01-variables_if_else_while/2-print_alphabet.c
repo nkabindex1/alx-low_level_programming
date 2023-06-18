26860815ee9f70df63ea5240c936f934bdeb92c7
Xolani <nkabindex1@gmail.com>
2023-06-18 12:54:13 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - point of entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'X';
+
+	putchar(tolower(ch));
+	putchar('\n');
+	return (0);
+}
