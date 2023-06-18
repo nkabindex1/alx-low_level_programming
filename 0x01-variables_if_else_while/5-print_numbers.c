db491649b7eef1f8b31fdeb4c32102240b0da531
Xolani <nkabindex1@gmail.com>
2023-06-18 13:47:50 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - point of entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
