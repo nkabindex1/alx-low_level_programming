d215bee7305043c02b3a99b16568f832ae239941
Xolani <nkabindex1@gmail.com>
2023-06-18 14:03:08 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - point of entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int length = sizeof(alphabets) / sizeof(alphabets[0]);
+	int i;
+
+	for (i = length - 1; i < length - 1; i--)
+	{
+		char lowerCh = tolower(alphabets[i]);
+
+		putchar(lowerCh);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
