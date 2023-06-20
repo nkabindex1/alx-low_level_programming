6d10eb4f3c5be882156de95be1824664a4ad5c1d
Xolani <nkabindex1@gmail.com>
2023-06-20 10:06:46 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void _islower(int);
+void _isalpha(int);
+void print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int jack_bauer(void);
+int times_table(void);
+int add(int,int);
+void print_to_98(int);
+void print_times_table(int);
