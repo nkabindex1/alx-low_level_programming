ca51fcecebe0bee20812326a58df3bffbd6435cf
Xolani <nkabindex1@gmail.com>
2023-06-20 11:42:59 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of n
+ * @n: evaluated character
+ * Return: n (last digit)
+ */
+int print_last_digit(int n)
+{
+	int lastDigit = n % 10;
+
+	return (lastDigit);
+}
