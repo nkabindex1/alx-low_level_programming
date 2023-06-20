2dec5e88f9e0cc39cc63ce2c6373ece69602ec74
Xolani <nkabindex1@gmail.com>
2023-06-20 10:23:27 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 121; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
