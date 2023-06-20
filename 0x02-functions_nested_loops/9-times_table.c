80954da11351fedc698c80f6ad1483cc051e22a6
Xolani <nkabindex1@gmail.com>
2023-06-20 12:46:39 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,7 +25,7 @@ void times_table(void)
 			if (j < 9)
 			{
 				_putchar(',');
-				tmp = i * (j + 1)
+				tmp = i * (j + 1);
                         	if (tmp > 9)
                                 	_putchar(' ');
                         	_putchar(' ');
