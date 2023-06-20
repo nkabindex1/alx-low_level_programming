bef8392250372c30aa32e44c8206e6c052ac2738
Xolani <nkabindex1@gmail.com>
2023-06-20 11:24:23 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for positives an negetives
+ * @n: evaluated character
+ * Return: 1 ifpositive, 0 if zero, -1 if negetive
+ */
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		_putchar(43);
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar(45);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int);
-void print_sign(int);
+int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
 int jack_bauer(void);
