10ba7eda6a55bd316601792eea2f97f945be56af
Xolani <nkabindex1@gmail.com>
2023-06-23 18:14:40 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints line with as long as the specified width
+ * @n: wdth of line
+ * Return: (Success)
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		int j;
+		for (j = 0; j <= n; j++)
+		{
+			if ( i == j && j != 0)
+				_putchar(47);
+			else
+				_putchar(32);
+		}
+		_putchar('\n');
+}
