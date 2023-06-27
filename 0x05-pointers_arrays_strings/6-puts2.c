7c1aeacee0b3e79e4ecdaac21dc44ad84c8f0ab2
Xolani <nkabindex1@gmail.com>
2023-06-27 15:45:08 -0700

    x

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints string and adds newline
+ * @s: string to be printed
+ */
+void puts2(char *str)
+{
+	int counter;
+	char *tmp;
+	int i;
+
+	counter = 0;
+	while (*s != '\0')
+	{
+		if (counter %  == 0)
+			_putchar(*s);
+		counter++;
+		s++;
+	}
+	_putchar('\n');
+}
