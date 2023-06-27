a7e4b7bd7969ef5de040496bfd83b5c535ee2100
Xolani <nkabindex1@gmail.com>
2023-06-27 14:09:19 -0700

    x

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - update pointer value to 98
+ * @a: pointer value to be changed
+ * @b: pointer to add
+ */
+void swap_int(int *a, int *b)
+{
+	*tmp = a;
+	*a = b;
+	*b = tmp;
+}
