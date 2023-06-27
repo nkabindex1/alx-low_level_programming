733a5227f09dcb3649c17276a766020d630ce714
Xolani <nkabindex1@gmail.com>
2023-06-27 15:03:54 -0700

    x

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _print_rev - prints string in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int counter;
+       
+	counter =0;
+	while (*s != '\0')
+	{
+		s++;
+		counter++;
+	}
+
+	while(counter)
+	{
+		_putchar(s[counter-1]);
+		counter--;
+	}
+	_putchar('\n');
+}
