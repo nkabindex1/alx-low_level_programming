b9063a44e8c035404f1338f5b3550b6df15db0a4
Xolani <nkabindex1@gmail.com>
2023-06-28 13:38:07 -0700

    0x06-pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *reverse_array - compare two strings
+ *@a: string one
+ *@n: number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int j;
+	int length = (int)(n / 2) + (n % 2); 
+
+	j = 0;
+	for (; j <= length; j++)
+	{
+		int tmp = a[j];
+
+		a[j] = a[n-j-1];
+		a[n-j-1] = tmp;
+	}
+}
