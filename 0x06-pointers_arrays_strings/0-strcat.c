2550a17f96f3dacb07b1ca94cee9279a691e6158
Xolani <nkabindex1@gmail.com>
2023-06-28 12:00:04 -0700

    0x06-pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strcat(char *dest, char *src) - concatenate two strings
+ *@dest: string one 
+ *@src: string two
+ *Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int counter;
+
+	while(*dest++)
+	{
+		counter++;
+	}
+	dest[counter] = *src;
+	return *dest[0];
+}
