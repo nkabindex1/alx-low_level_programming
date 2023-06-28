39d2e7db05900919208943c36f1b20bdd5604a6f
Xolani <nkabindex1@gmail.com>
2023-06-28 12:53:13 -0700

    0x06-pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strncpy - concatenate two strings
+ *@dest: string one
+ *@src: string two
+ *@n: bytes
+ *Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	/*int i;*/
+	int j;
+
+	/**i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	*/
+	j = 0;
+	for (; j < n; j++)
+	{
+		if (src[j] == '\0')
+			break;
+		dest[j] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
