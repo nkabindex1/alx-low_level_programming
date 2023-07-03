179d6462bf96a6bc30619a1f354bc93d1c7bdd72
Xolani <nkabindex1@gmail.com>
2023-07-03 15:03:09 -0700

    changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,18 +2,21 @@
 /**
  * _strchr - copies address from src to dest
  * @s: array being searched
- * @c: character being evaluated 
- * Return: address of the character with the first occurance of the evaluated char
+ * @c: character being evaluated
+ * Return: address of the char c
  */
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 	char *ptr;
-	
+
 	while (s[i] != '\0')
 	{
-		if ( s[i] == c)
+		if (s[i] == c)
+		{
 			ptr = &s[i];
+			break;
+		}
 	}
 	return (ptr);
 }
