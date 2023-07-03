00f7f35de8dbeb7de8befd2597d02cd1b05e7ba3
Xolani <nkabindex1@gmail.com>
2023-07-03 14:57:11 -0700

    changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _memset - sets a block of memory with n char letters
- * @*s: array s
+ * @s: array s
  * @b: char letter that will assigned to space
- * @n: number of char letters 
+ * @n: number of char letters
+ * Return: array of char  b
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-
 
 	for (i = 0; i < n; i++)
 	{
