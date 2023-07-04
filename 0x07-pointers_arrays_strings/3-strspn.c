b926cd720f2eb4cb5a1e3ef01de1973e2d69c5de
Xolani <nkabindex1@gmail.com>
2023-07-04 12:53:45 -0700

    changes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,19 +7,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n;
+	unsigned int n, r;
 
 	n = 0;
 	while (*s)
 	{
-		for (r = 0; accepr[r]; r++)
+		for (r = 0; accept[r]; r++)
 		{
 			if (*s == accept[r])
 			{
 				n++;
 				break;
 			}
-			else if (accepr[r + 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
