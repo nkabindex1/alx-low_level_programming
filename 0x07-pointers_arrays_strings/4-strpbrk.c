e5a7e20f4da88b6e82c9f90f55e017d15344c946
Xolani <nkabindex1@gmail.com>
2023-07-04 13:00:44 -0700

    changes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - locates first occurance of a word
+ * @s: array being searched
+ * @accept: character being evaluated
+ * Return: number of bytes from accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int n, r;
+
+	n = 0;
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
