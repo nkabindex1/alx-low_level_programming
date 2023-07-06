239637d285299148c754c6b70e1818f73881386c
Xolani <nkabindex1@gmail.com>
2023-07-06 01:36:55 -0700

    changes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,29 @@
 #include "main.h"
 /**
+ * is_prime - Entry point
+ * @n: number
+ * @divisor: number
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime(int n, int divisor);
+
+int is_prime(int n, int divisor)
+{
+	if (divisor == 1)
+		return (1);
+	else if (n % divisor == 0)
+		return (0);
+	return is_prime(n, divisor - 1);
+}
+
+
+/**
  * is_prime_number - Entry point
  * @n: int input
  * Return: 1 if prime number, 0
  */
 int is_prime_number(int n)
 {
-	return (is_prime(n, n-1));
+	return (is_prime(n, n - 1));
 }
-/**
- * is_prime - Entry point
- * @n: number
- * @divisor: number
- * Return: 1 if prime, 0 if not
- */
-int is_prime(int n, int divisor)
-{
-	if (divisor == 1)
-		return (1);
-	else if (n % divisor == 0)
-       		return (0);
-	return is_prime(n , divisor - 1);	
-}
+
