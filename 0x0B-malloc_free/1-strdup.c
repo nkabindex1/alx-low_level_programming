8db73af82ab993036e77c1a7dba237da08899f4f
Xolani <nkabindex1@gmail.com>
2023-07-10 05:01:05 -0700

    final changes

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strdup - allocate new memory for string
+ *@str: array
+ *Return: address of the array
+ */
+
+char *_strdup(char *str)
+{
+	char *tmp;
+	unsigned int i, r;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+
+	for (; str[i] != '\0'; i++)
+		continue;
+	tmp = malloc(sizeof(char) * (i + 1));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	for (r = 0;str[r]; r++)
+	{
+		tmp[r] = str[r];
+	}
+	return (tmp);
+}
