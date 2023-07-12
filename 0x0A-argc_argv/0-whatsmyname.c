e219799645ef4811c05a8c31193746463242a5bc
Xolani <nkabindex1@gmail.com>
2023-07-12 14:04:28 -0700

    final changes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - print the name of program
+ *@argc: input
+ *@argv: input
+ *Return: Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - print the name of program
+ *@argc: input
+ *@argv: input
+ *Return: Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
