e9bc0c04b2f687f637653a223554f098a3b9e355
Xolani <nkabindex1@gmail.com>
2023-07-13 11:47:25 -0700

    final changes

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *string_nconcat - creates space in memory
+ *@s1: str1
+ *@s2: str2
+ *@n: amount to input
+ *Return: pointer to concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	void *ptr;
+	int i, r, a;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		continue
+	ptr = malloc(i + b + 1);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		exit(98);
+	}
+
+	for (r = 0; r <= i; r++)
+		ptr[r] = s1[r];
+	for (a = 0; a <= n && s2[a] != '\0'; a++)
+		r++;
+		ptr[r + a] = s2[a];
+	return (ptr);
+}
