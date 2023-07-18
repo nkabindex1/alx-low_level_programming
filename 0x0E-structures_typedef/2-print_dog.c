7c1baa313a9199fe76fbadd009a7ff97025144fe
Xolani <nkabindex1@gmail.com>
2023-07-17 17:38:54 -0700

    final changes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,15 @@
 #include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints the struct of dog
+ * @d: struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf
+	}
+}
+
