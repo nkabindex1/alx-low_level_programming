82623b3de30b0585a57da6086b42d154d10c83d7
Xolani <nkabindex1@gmail.com>
2023-07-18 12:27:10 -0700

    final changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,75 @@
 #include "dog.h"
+/**
+ * _strlen - return the length of a string
+ * @s: evaluated string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copies string from src to dest
+ * @src: pointer to string to be copied
+ * @dest: pointer to buffer in which we copy place string
+ * Return: pointer to dst
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - create new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Return: dog of type dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * (len1 +1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+
+}
