8755a15710e74f5c1e6c3badb803abb23f7256ab
Xolani <nkabindex1@gmail.com>
2023-07-19 13:42:57 -0700

    final changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  * @size: no of elements to print
  * @action: pointer to print in regular or hex
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
