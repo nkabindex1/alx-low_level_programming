785ddd7e12d9dccb2da9f743a9ac45f51c7df454
Xolani <nkabindex1@gmail.com>
2023-07-19 14:29:45 -0700

    final changes

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+/**
+ * main - entry point
+ * @array: array
+ * @size: no of elements to print
+ * @cmp: pointer to one of the functions
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[i] != '\0')
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
