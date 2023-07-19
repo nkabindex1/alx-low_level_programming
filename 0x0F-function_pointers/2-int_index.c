68027a28d83d7b321d5be0c4e9476afc376969af
Xolani <nkabindex1@gmail.com>
2023-07-19 13:40:08 -0700

    final changes

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - prints name
+ * @array: array
+ * @size: no of elements to print
+ * @cmp: pointer to one of the functions
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0 ; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
